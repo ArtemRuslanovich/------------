1fb6ecc2e0860109ef1687708150c170fc87f6cd
Artiom Zavatski <artiomzavatski@gmail.com>
2024-05-25 15:06:20 +0200

    sqlite problem fix

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ api_id = '28880373'
 api_hash = '8ac84cce1fec1e3aeb2afc2140cb85e5'
 phone_number = '+48507942372'
 
-client = TelegramClient('session_name', api_id, api_hash)
+client = TelegramClient(None, api_id, api_hash)
 
 async def main():
     await client.start(phone_number)
 
-    @client.on(events.NewMessage)
-    async def handle_new_message(event):
-        if 'missed call' in event.raw_text.lower() or 'missed video call' in event.raw_text.lower():
-            sender = await event.get_sender()
-            sender_id = sender.id
-            await client.send_message(sender_id, 'Извините, я не могу сейчас ответить на звонок. Пожалуйста, оставьте сообщение.')
+    @client.on(events.NewMessage(pattern='(missed call|missed video call)', flags=events.NewMessage.Incoming))
+    async def handle_missed_call(event):
+        sender = await event.get_sender()
+        sender_id = sender.id
+        message = 'Здравствуйте 👋, это сообщение отправлено автоматом 🤖, в данный момент я не абонент 😴, оставьте сообщение👇.'
+        await client.send_message(sender_id, message)
 
     print("Бот автоответчик запущен...")
     await client.run_until_disconnected()
 
-# Запуск основной функции
 asyncio.run(main())
